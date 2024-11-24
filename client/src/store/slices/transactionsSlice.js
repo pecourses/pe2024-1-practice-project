784c4196d85fa9b7a3846c4ce41a0973a1818d57
Viktoriia <freshitpte@gmail.com>
2024-11-24 15:51:34 +0200

    Refactor transactionSlice

--- client/src/store/slices/transactionsSlice.js
+++ client/src/store/slices/transactionsSlice.js
@@ -1,5 +1,10 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import * as restController from '../../api/rest/restController';
+import {
+  decorateAsyncThunk,
+  pendingReducer,
+  rejectedReducer,
+} from '../../utils/store';
 
 const TRANSACTIONS_SLICE_NAME = 'transactions';
 
@@ -9,37 +14,30 @@ const initialState = {
   error: null,
 };
 
-// /transactions/get
-export const getTransactionsThunk = createAsyncThunk(
-  `${TRANSACTIONS_SLICE_NAME}/get`,
-  async (payload, { rejectWithValue }) => {
-    try {
-      const { data } = await restController.getTransactions();
-      return data;
-    } catch (err) {
-      return rejectWithValue(err);
-    }
-  }
-);
+export const getTransactionsThunk = decorateAsyncThunk({
+  key: `${TRANSACTIONS_SLICE_NAME}/get`,
+  thunk: async () => {
+    const { data } = await restController.getTransactions();
+    return data;
+  },
+});
+
+const reducers = {};
+
+const extraReducers = builder => {
+  builder.addCase(getTransactionsThunk.pending, pendingReducer);
+  builder.addCase(getTransactionsThunk.fulfilled, (state, { payload }) => {
+    state.isFetching = false;
+    state.transactions = [...payload];
+  });
+  builder.addCase(getTransactionsThunk.rejected, rejectedReducer);
+};
 
 const transactionsSlice = createSlice({
   name: TRANSACTIONS_SLICE_NAME,
   initialState,
-  reducers: {},
-  extraReducers: builder => {
-    builder.addCase(getTransactionsThunk.pending, state => {
-      state.isFetching = true;
-      state.error = null;
-    });
-    builder.addCase(getTransactionsThunk.fulfilled, (state, { payload }) => {
-      state.isFetching = false;
-      state.transactions = [...payload];
-    });
-    builder.addCase(getTransactionsThunk.rejected, (state, { payload }) => {
-      state.isFetching = false;
-      state.error = payload;
-    });
-  },
+  reducers,
+  extraReducers,
 });
 
 const { reducer, actions } = transactionsSlice;
